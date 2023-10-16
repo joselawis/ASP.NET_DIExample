868858763419ebc62048aacc7a90561d037a5e53
joseluiscontreras <joseluis.contreras@innocv.com>
2023-10-16 18:05:25 +0200

    Transient Example

--- Services/CitiesService.cs
+++ Services/CitiesService.cs
@@ -3,10 +3,12 @@ namespace Services;
 
 public class CitiesService : ICitiesService
 {
+    private Guid _serviceInstanceId;
     private List<string> _cities;
-
+    public Guid ServiceInstanceId { get { return _serviceInstanceId; } }
     public CitiesService()
     {
+        _serviceInstanceId = Guid.NewGuid();
         _cities = new List<string>(){
             "London",
             "Paris",
