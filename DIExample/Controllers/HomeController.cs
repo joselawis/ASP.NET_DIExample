e65c999147d47d6dffce21d590f3c82344e2e309
joseluiscontreras <joseluis.contreras@innocv.com>
2023-10-16 17:50:21 +0200

    Method injection FromServices

--- DIExample/Controllers/HomeController.cs
+++ DIExample/Controllers/HomeController.cs
@@ -5,15 +5,17 @@ namespace DIExample.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ICitiesService _citiesService;
+        // private readonly ICitiesService _citiesService;
 
+        /*
         public HomeController(ICitiesService citiesService)
         {
             _citiesService = citiesService; //new CitiesService();
         }
+        */
 
         [Route("/")]
-        public IActionResult Index()
+        public IActionResult Index([FromServices] ICitiesService _citiesService)
         {
             List<string> cities = _citiesService.GetCities();
             return View(cities);
