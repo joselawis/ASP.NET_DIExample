ec05690651634f3db81478d2f4e628456ecc1279
joseluiscontreras <joseluis.contreras@innocv.com>
2023-10-16 18:24:00 +0200

    AddScope for short definition

--- DIExample/Program.cs
+++ DIExample/Program.cs
@@ -3,11 +3,7 @@ using Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
-builder.Services.Add(new ServiceDescriptor(
-    typeof(ICitiesService),
-    typeof(CitiesService),
-    ServiceLifetime.Scoped
-));
+builder.Services.AddScoped<ICitiesService, CitiesService>();
 
 var app = builder.Build();
 
