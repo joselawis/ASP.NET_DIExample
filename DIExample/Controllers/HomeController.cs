b630a4708f959e5ab12a6107fbd12c7377a6df83
joseluiscontreras <joseluis.contreras@innocv.com>
2023-10-17 18:00:26 +0200

    Autofac

--- DIExample/Controllers/HomeController.cs
+++ DIExample/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Autofac;
 using Microsoft.AspNetCore.Mvc;
 using ServiceContracts;
 
@@ -8,14 +9,18 @@ namespace DIExample.Controllers
         private readonly ICitiesService _citiesService1;
         private readonly ICitiesService _citiesService2;
         private readonly ICitiesService _citiesService3;
-        private readonly IServiceScopeFactory _serviceScopeFactory;
+        private readonly ILifetimeScope _lifeTimeScope;
 
-        public HomeController(ICitiesService citiesService1, ICitiesService citiesService2, ICitiesService citiesService3, IServiceScopeFactory serviceScopeFactory)
+        public HomeController(
+            ICitiesService citiesService1,
+            ICitiesService citiesService2,
+            ICitiesService citiesService3,
+            ILifetimeScope lifetimeScope)
         {
             _citiesService1 = citiesService1; //new CitiesService();
             _citiesService2 = citiesService2;
             _citiesService3 = citiesService3;
-            _serviceScopeFactory = serviceScopeFactory;
+            _lifeTimeScope = lifetimeScope;
         }
 
 
@@ -27,10 +32,10 @@ namespace DIExample.Controllers
             ViewBag.InstanceId_CitiesService_2 = _citiesService2.ServiceInstanceId;
             ViewBag.InstanceId_CitiesService_3 = _citiesService3.ServiceInstanceId;
 
-            using (IServiceScope scope = _serviceScopeFactory.CreateScope())
+            using (ILifetimeScope scope = _lifeTimeScope.BeginLifetimeScope())
             {
                 // Inject CitiesService
-                ICitiesService citiesService = scope.ServiceProvider.GetRequiredService<ICitiesService>();
+                ICitiesService citiesService = scope.Resolve<ICitiesService>();
                 // DB Work
                 ViewBag.InstanceId_CitiesService_InScope = citiesService.ServiceInstanceId;
 
--- DIExample/Program.cs
+++ DIExample/Program.cs
@@ -1,9 +1,23 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using ServiceContracts;
 using Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
 builder.Services.AddControllersWithViews();
-builder.Services.AddScoped<ICitiesService, CitiesService>();
+builder.Host.ConfigureContainer<ContainerBuilder>(
+    containerBuilder =>
+    {
+        containerBuilder.RegisterType<CitiesService>()
+        .As<ICitiesService>()
+        // .InstancePerDependency(); // Add Transient
+        .InstancePerLifetimeScope(); // Add Scoped
+        // .SingleInstance(); // Add Singleton
+    }
+);
 
 var app = builder.Build();
 
